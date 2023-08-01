5e128874950d3122d7958577a573ab1336f09ad5
sulaxsan <sulaxsanlathan@gmail.com>
2023-08-01 11:36:21 +0530

    second commit

--- src/component/landing.js
+++ src/component/landing.js
@@ -13,7 +13,7 @@ function Landing() {
     const [userId, setUserId] = useState('');
     const [selectedImage, setSelectedImage] = useState(null);
     const [selectImage, setSelectImage] = useState(null);
-
+    const [result, setResult] = useState(null)
 
 
 
@@ -29,11 +29,6 @@ function Landing() {
             });
     }, [])
 
-
-
-
-
-
     const handleImageChange = (event) => {
         const file = event.target.files[0];
         setSelectImage(file)
@@ -43,11 +38,9 @@ function Landing() {
             reader.onloadend = () => {
                 setSelectedImage(reader.result);
             };
-
             reader.readAsDataURL(file);
         }
     };
-
     const Uploadimage = async () => {
         try {
             const formData = new FormData();
@@ -55,15 +48,41 @@ function Landing() {
             formData.append('userid', userDetail.user._id);
             //   console.log(selectImage)
             await axios.post('http://localhost:5000/api/user/upload', formData)
-            .then((res)=>{
-                console.log(res.data)
-            })
+                .then((res) => {
+                    console.log(res.data)
+                })
         } catch (error) {
             console.error(error);
         }
-
     }
+    const getResults = async () => {
+        try {
+            const formData = new FormData();
+            formData.append('file', selectImage);
+            const response = await axios.post('http://f736-35-197-93-16.ngrok.io/makeup', formData)
+            const blob = new Blob([response.data], { type: 'image/jpeg' });
+            const imageUrl = URL.createObjectURL(blob);
+            setResult(imageUrl)
 
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    const isBase64 = (str) => {
+        try {
+            return btoa(atob(str)) === str;
+        } catch (err) {
+            return false;
+        }
+    };
+    const normalizeBase64Data = (data) => {
+        const base64Prefix = 'data:image/jpeg;base64,';
+        if (data.startsWith(base64Prefix)) {
+            return data.slice(base64Prefix.length);
+        } else {
+            return data;
+        }
+    };
     return (
 
         <MDBContainer fluid >
@@ -128,7 +147,7 @@ function Landing() {
                                 <div className="center card  m-1 shadow">
 
                                     <div className="pb-4">
-                                        <button type="button" className="btn btn-outline-dark mt-4 ">Generate Image</button>
+                                        <button type="button" className="btn btn-outline-dark mt-4 " onClick={getResults}>Generate Image</button>
                                     </div>
                                 </div>
                             </div>
@@ -139,12 +158,22 @@ function Landing() {
 
                 <MDBCol md="12" lg="6" className="mb-5">
                     <MDBCard className='shadow '>
-
                         <div>
                             <div className="frame ">
                                 <div className="center card p-5  m-1 shadow">
                                     <div className="title m-5 pb-5">
                                         <h1>View Processed Image</h1>
+                                        {
+                                            console.log(result)
+                                        }
+                                        {result && (
+                                            <img
+                                                // src={`data:image/jpeg;base64,${result}`}
+                                                src={result}
+                                                alt="Preview"
+                                                style={{ maxWidth: '400px', maxHeight: '500px' }}
+                                            />
+                                        )}
                                     </div>
 
                                     <div className="dropzone pb-5 mb-2">
@@ -171,8 +200,6 @@ function Landing() {
                                         <MDBCardImage style={{ height: '250px', width: '250px' }}
                                             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ06NJPcUDJbB9wdhEXbFdn3nH4fJOTAvYc_C6dXzXs1pBRgEWgmaiwdgz5Ux7AMIC7nPA&usqp=CAU"
                                             position="top"
-
-
                                         />
                                     </div>
                                     <Link to="/products" style={{ textDecoration: 'none' }}> <button className="btn btn-outline-dark mt-3 mx-2">More Products</button> </Link>
