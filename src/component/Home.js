932895af714faa1406ece2d86483a12a3ef38843
sulaxsan <sulaxsanlathan@gmail.com>
2023-08-17 21:19:08 +0530

    fourthd commit

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,50 +1,4 @@
-// import React from 'react';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import '../App.css'
-// import anime from 'animejs'
-// import { Link, useNavigate } from 'react-router-dom';
-// // import { Link, useNavigate } from 'react-router-dom';
-// // import { Formik, Form, Field } from 'formik';
-// // import * as Yup from 'yup';
-// // // import { Register } from './AxiosApi/FetchCalls';
-// // import axios from 'axios'
-// // import { toast } from 'react-toastify';
-// function Home() {
 
-    
-//     const navigate = useNavigate();
-//     return (
-//         <div style={{ 
-//         display: 'flex',
-// height:'100%',
-// backgroundColor:"#fbe3e8",alignItems:"center"
-//          }}>
-//             <div style={{ width: '60%',alignItems:"center",margin:'50px'}} >
-//                 <img
-//                     src='https://img.businessoffashion.com/resizer/_h8NfpNMD-L8pSDH-cOlX478Fh0=/1600x900/filters:format(jpg):quality(70):focal(-5x45:5x55)/cloudfront-eu-central-1.images.arcpublishing.com/businessoffashion/2UDNDDYDAZB57EYGS55VRAW7AQ.jpg'
-//                     style={{  width: '100%', 
-//                     // minHeight:'100vh'
-//                     height:"90%",
-//                      }} />
-//             </div>
-//             <div style={{ width: '40%'}} >
-//                 <div className="grid">
-// 					<figure className="effect-dexter">
-// 						<img src="https://c0.wallpaperflare.com/preview/506/9/180/beautiful-beauty-cute-elegant.jpg" alt="img19" />
-// 						<figcaption>
-// 							<h2>Try On<span> Make Up</span></h2>
-// 							<p  onClick={()=>  navigate('/login')}>Try Here</p>
-// 						</figcaption>			
-// 					</figure>
-					
-// 				</div>
-//             </div>
-//             <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
-//         </div>
-//     )
-// }
-
-// export default Home;
 
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -58,14 +12,17 @@ function Home2() {
     const navigate = useNavigate();
     return (
         <div >
+         <div className='row'>
+         <h3 style={{color:'black',textAlign:"center",marginTop:'10px',fontWeight:"800"}}>Try On Make Up</h3>
+            </div>
             <div className='row m-5'>
-                <div className='col-lg-8 text-dark  mb-5 ' style={{display:'flex', flexDirection:"column"}}>
+                <div className='col-lg-8 text-dark  mb-5 ' style={{display:'flex', flexDirection:"column",}}>
                     <img src="https://images.lifestyleasia.com/wp-content/uploads/sites/7/2022/02/11183032/CtFRRyg7ekq5Jd4jizS2WZ-2-1600x900.jpg?tr=w-1366" className="mt-5" style={{}} alt="Responsive image" />
                 </div>
-                <div className='col-lg-4 text-dark bg-secondary'>
+                <div className='col-lg-4 text-dark '  style={{backgroundColor:'#9CA8B2'}}>
                 
-                    <p className='text-white  para m-3' style={{fontSize:'20px'}}> 
-                    <h3>Try On Make Up</h3>
+                    <p className='text-white  para m-3' style={{fontSize:'20px',}}> 
+                    
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur fermentum id turpis id porttitor.
                         In eget efficitur enim. Pellentesque fringilla ex vitae est facilisis, nec dignissim metus pellentesque.
                         Nam commodo est maximus arcu tincidunt interdum. In fringilla, sapien ut ultrices gravida, libero dolor auctor enim,
